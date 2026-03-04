4f067592067617a5afe7214f7de3ba6db0568e8f
CyberSanderson <cyberopsanderson@gmail.com>
2026-03-03 22:10:25 -0500

    updated the agent

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import history from 'connect-history-api-fallback';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      appType: 'spa',
       server: {
         port: 3000,
         host: '0.0.0.0',
         fs: {
           strict: false
         },
-        historyApiFallback: {
-          disableDotRule: true,
-        }, // Correct SPA fallback for Vite
+        middlewareMode: false,
+        middleware: [history()],
       },
       plugins: [react()],
       define: {
